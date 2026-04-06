420d5befc60cfd338f6ffe25a7958a00bf5ad941
Hadi Moshayedi <hadi@ubicloud.com>
2026-04-06 15:39:39 -0700

    Remove usages of `VmPool.storage_encrypted`
    We have been using encrypted storage for all GitHub Runner VMs for a
    long time, and in 13a7fa3 the storage layer removed support for
    unencrypted writable volumes completely. Therefore,
    `VmPool.storage_encrypted` can be dropped now. This commit removes uses
    of that column so we can drop it in a follow-up migration.

--- prog/vm/vm_pool.rb
+++ prog/vm/vm_pool.rb
@@ -5,8 +5,7 @@ require_relative "../../lib/net_ssh"
 class Prog::Vm::VmPool < Prog::Base
   subject_is :vm_pool
 
-  def self.assemble(size:, vm_size:, boot_image:, location_id:, storage_size_gib:,
-    storage_encrypted:, arch:)
+  def self.assemble(size:, vm_size:, boot_image:, location_id:, storage_size_gib:, arch:)
     DB.transaction do
       vm_pool = VmPool.create(
         size:,
@@ -14,7 +13,6 @@ class Prog::Vm::VmPool < Prog::Base
         boot_image:,
         location_id:,
         storage_size_gib:,
-        storage_encrypted:,
         arch:,
       )
       Strand.create_with_id(vm_pool, prog: "Vm::VmPool", label: "create_new_vm")
@@ -24,7 +22,7 @@ class Prog::Vm::VmPool < Prog::Base
   label def create_new_vm
     storage_params = {
       size_gib: vm_pool.storage_size_gib,
-      encrypted: vm_pool.storage_encrypted,
+      encrypted: true,
     }
     ps = Prog::Vnet::SubnetNexus.assemble(
       Config.vm_pool_project_id,
